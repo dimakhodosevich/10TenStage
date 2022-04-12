f96f5c581f9eba04b95aa80688aba6bc0ac18e10
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-12 22:22:15 +0300

    Initial commit

---
+++ src/by/itstep/khodosevich/tenproject/task01/module/logic/SameValue.java
@@ -0,0 +1,13 @@
+package by.itstep.khodosevich.tenproject.task01.module.logic;
+
+public final class SameValue {
+
+    private SameValue(){
+
+    }
+
+    public static boolean checkTheSameValue(double ... numbers){
+        return true;
+    }
+
+}
---
+++ test/by.itstep.khodosevich.tenproject/task01/TestSameValue.java
@@ -0,0 +1,9 @@
+package by.itstep.khodosevich.tenproject.task01;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static by.itstep.khodosevich.tenproject.task01.module.logic.SameValue.*;
+
+public class TestSameValue {
+    
+}

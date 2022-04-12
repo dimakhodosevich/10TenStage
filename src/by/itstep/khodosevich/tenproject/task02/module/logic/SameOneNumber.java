6632899eb6b03f3253374050c6ccb393b0388430
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-12 23:16:30 +0300

    Make new classes for task02. Add SameOneNumber class and test class for it. Make unit test.

---
+++ src/by/itstep/khodosevich/tenproject/task02/module/logic/SameOneNumber.java
@@ -0,0 +1,11 @@
+package by.itstep.khodosevich.tenproject.task02.module.logic;
+
+
+public class SameOneNumber {
+
+    private SameOneNumber(){}
+
+    public static boolean checkTheSameOneNumber(double number){
+        return true;
+    }
+}

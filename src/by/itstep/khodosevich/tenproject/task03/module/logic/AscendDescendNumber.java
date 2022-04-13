70f1baec8699a08701f38fc28503fa7067365d53
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-13 15:26:37 +0300

    Add logic to AscendDescend class. Test it. All works well.

--- src/by/itstep/khodosevich/tenproject/task03/module/logic/AscendDescendNumber.java
+++ src/by/itstep/khodosevich/tenproject/task03/module/logic/AscendDescendNumber.java
@@ -7,7 +7,66 @@ public class AscendDescendNumber {
     }
 
     public static boolean checkAscendDescendNumber(double number){
+        int numberInt = (int)number;
 
-        return true;
+        boolean isAscendValue = checkAscendNumber(numberInt);
+        boolean isDescendValue = checkDescendNumber(numberInt);
+
+        if(isAscendValue){
+            return isAscendValue;
+        } else if(isDescendValue)
+        {
+            return isDescendValue;
+        }
+
+        boolean isNotAscendAndDescend = isAscendValue;
+
+        return isNotAscendAndDescend;
     }
+
+    private static boolean checkAscendNumber(double number){
+        int numberInt = (int)number;
+
+        if(numberInt<=9){
+            throw new RuntimeException();
+        }
+
+        boolean isAscend = true;
+
+        int lastNumber = numberInt%10;
+        numberInt/=10;
+
+        while (numberInt>0){
+            int previousNumber = numberInt%10;
+            numberInt/=10;
+
+            if(lastNumber<previousNumber){
+                return !isAscend;
+            }
+            lastNumber = previousNumber;
+        }
+
+        return isAscend;
+    }
+
+    private static boolean checkDescendNumber(double number){
+        int numberInt = (int)number;
+        boolean isDescend = true;
+
+        int lastNumber = numberInt%10;
+        numberInt/=10;
+
+        while (numberInt>0){
+            int previousNumber = numberInt%10;
+            numberInt/=10;
+
+            if(lastNumber>previousNumber){
+                return !isDescend;
+            }
+            lastNumber = previousNumber;
+        }
+
+        return isDescend;
+    }
+
 }

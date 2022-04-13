65e69f7537ac77328902f4c35435d4d306c8b5ef
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-13 15:06:38 +0300

    Make new package. Add AscendDescend class, make plug method, add logic to test TestAscendDescend class.

---
+++ src/by/itstep/khodosevich/tenproject/task03/module/logic/AscendDescendNumber.java
@@ -0,0 +1,13 @@
+package by.itstep.khodosevich.tenproject.task03.module.logic;
+
+public class AscendDescendNumber {
+
+    private AscendDescendNumber(){
+
+    }
+
+    public static boolean checkAscendDescendNumber(double number){
+
+        return true;
+    }
+}
---
+++ test/by/itstep/khodosevich/tenproject/task03/TestAscendDescendNumber.java
@@ -0,0 +1,56 @@
+package by.itstep.khodosevich.tenproject.task03;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static by.itstep.khodosevich.tenproject.task03.module.logic.AscendDescendNumber.*;
+
+public class TestAscendDescendNumber {
+
+    public static final int isAscendNumber;
+    public static final int notAscendNumber;
+    public static final int isDescendNumber;
+    public static final int notDescendNumber;
+    public static final int oneNumber;
+    public static String errorMessage;
+
+    static {
+        isAscendNumber = 123456;
+        notAscendNumber = 123123;
+        isDescendNumber = 654321;
+        notDescendNumber = 654654;
+        oneNumber = 7;
+        errorMessage = "Your method works bad!!!";
+    }
+
+    @Test
+    public void testCheckAscendDescendNumberIsAscend() {
+        boolean actual = checkAscendDescendNumber(isAscendNumber);
+        assertTrue(errorMessage, actual);
+    }
+
+    @Test
+    public void testCheckAscendDescendNumberNotAscend() {
+        boolean actual = checkAscendDescendNumber(notAscendNumber);
+        assertFalse(errorMessage, actual);
+    }
+
+    @Test
+    public void testCheckAscendDescendNumberIsDescend() {
+        boolean actual = checkAscendDescendNumber(isDescendNumber);
+        assertTrue(errorMessage, actual);
+    }
+
+    @Test
+    public void testCheckAscendDescendNumberNotDescend() {
+        boolean actual = checkAscendDescendNumber(notDescendNumber);
+        assertFalse(errorMessage, actual);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCheckAscendDescendNumberOneNumber() {
+        checkAscendDescendNumber(oneNumber);
+    }
+
+
+}

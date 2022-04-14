7befd7e1cd9657a82a101457055d930f82e3c909
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-14 18:09:25 +0300

    Add ParityCounter, make test for it.

---
+++ src/by/itstep/khodosevich/tenproject/task08/module/logic/ParityCounter.java
@@ -0,0 +1,12 @@
+package by.itstep.khodosevich.tenproject.task08.module.logic;
+
+public class ParityCounter {
+
+    private ParityCounter(){
+
+    }
+
+    public static int countParityNumber(double number){
+        return -1;
+    }
+}

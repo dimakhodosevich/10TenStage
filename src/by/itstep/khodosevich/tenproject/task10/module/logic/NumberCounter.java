fe8877b754c7ec01764710b9089458c5cb3506bd
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-14 20:41:51 +0300

    Create test and plug method for count number in value.

---
+++ src/by/itstep/khodosevich/tenproject/task10/module/logic/NumberCounter.java
@@ -0,0 +1,18 @@
+package by.itstep.khodosevich.tenproject.task10.module.logic;
+
+public class NumberCounter {
+
+    private static final String ERROR_MESSAGE;
+
+    static {
+        ERROR_MESSAGE = "Your value lower than 1.\n";
+    }
+
+    private NumberCounter(){
+
+    }
+
+    public static int countNumberInValue(double value, int number){
+        return -1;
+    }
+}

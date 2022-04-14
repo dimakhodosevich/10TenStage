97bfdb4fbde46731008127bb85269ff69f8dbc98
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-14 16:55:31 +0300

    Make logic for ParityNumber class, test it.

--- src/by/itstep/khodosevich/tenproject/task06/module/logic/ParityNumber.java
+++ src/by/itstep/khodosevich/tenproject/task06/module/logic/ParityNumber.java
@@ -1,9 +1,23 @@
 package by.itstep.khodosevich.tenproject.task06.module.logic;
 
 public class ParityNumber {
-    private ParityNumber(){}
+    private ParityNumber() {
+    }
 
-    public static boolean checkParityNumber(double number){
-        return true;
+    public static boolean checkParityNumber(double number) {
+        int numberInt = (int) number;
+        boolean result = true;
+
+        while (numberInt > 0) {
+
+            int lastNumber = numberInt % 10;
+            numberInt /= 10;
+
+            if (lastNumber % 2 != 0) {
+                return !result;
+            }
+        }
+
+        return result;
     }
 }

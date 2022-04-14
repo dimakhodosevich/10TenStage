78650a318ac0a2289efa52bd55b31cbaf126143d
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-14 16:50:22 +0300

    Add ParityNumber class and plug method, make test for it.

---
+++ src/by/itstep/khodosevich/tenproject/task06/module/logic/ParityNumber.java
@@ -0,0 +1,9 @@
+package by.itstep.khodosevich.tenproject.task06.module.logic;
+
+public class ParityNumber {
+    private ParityNumber(){}
+
+    public static boolean checkParityNumber(double number){
+        return true;
+    }
+}

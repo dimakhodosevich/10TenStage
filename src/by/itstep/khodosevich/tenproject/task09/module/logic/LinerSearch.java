6f5e5b841339cdd1112cae5ace721d22f0c643b2
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-14 18:51:37 +0300

    Add LinerSearch and test for it

---
+++ src/by/itstep/khodosevich/tenproject/task09/module/logic/LinerSearch.java
@@ -0,0 +1,13 @@
+package by.itstep.khodosevich.tenproject.task09.module.logic;
+
+public class LinerSearch {
+
+
+    private LinerSearch(){
+
+    }
+
+    public static int getMaxNumber(double number){
+        return -1;
+    }
+}

6147380e7721d55ceaa736623e9d602cb5fd7835
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-14 17:16:29 +0300

    Add plug to NonParityNumber, make test for it.

---
+++ src/by/itstep/khodosevich/tenproject/task07/module/logic/NonParityNumber.java
@@ -0,0 +1,11 @@
+package by.itstep.khodosevich.tenproject.task07.module.logic;
+
+public class NonParityNumber {
+    private NonParityNumber(){
+
+    }
+
+    public static boolean checkNonParityNumber(double number){
+        return true;
+    }
+}

fcc218d8a993a8b9d78ee27856b1224add7a8e99
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-14 20:58:00 +0300

    Add controller, add verification for searchNumber and add test for it.

--- test/by/itstep/khodosevich/tenproject/task10/TestNumberCounter.java
+++ test/by/itstep/khodosevich/tenproject/task10/TestNumberCounter.java
@@ -1,13 +1,14 @@
 package by.itstep.khodosevich.tenproject.task10;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 import static by.itstep.khodosevich.tenproject.task10.module.logic.NumberCounter.*;
 
 public class TestNumberCounter {
 
     @Test
-    public void testCountNumberInValuePositive(){
+    public void testCountNumberInValuePositive() {
         int value = 223344;
         int number = 2;
         int expected = 2;
@@ -17,19 +18,26 @@ public class TestNumberCounter {
     }
 
     @Test
-    public void testCountNumberInValueWithOne(){
+    public void testCountNumberInValueWithOne() {
         int value = 1;
         int number = 1;
-        int expected =1;
+        int expected = 1;
         int actual = countNumberInValue(value, number);
 
         assertEquals(expected, actual);
     }
 
-    @Test (expected = RuntimeException.class)
-    public void testCountNumberInValueWithZero(){
+    @Test(expected = RuntimeException.class)
+    public void testCountNumberInValueWithZero() {
         int value = 0;
         int number = 2;
+        int actual = countNumberInValue(value, number);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCountNumberInValueWithNegativeSearchNumber() {
+        int value = 342;
+        int number = -1;
         int actual = countNumberInValue(value, number);
     }
 
